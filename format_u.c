eb835bcc3e02cb64e8b8567389934f6b185d90da
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-12 15:26:47 -0700

    Bugged file

--- format_u.c
+++ format_u.c
@@ -8,8 +8,7 @@
  */
 int format_u(unsigned int n)
 {
-	size_t j, len, var, count;
-	char *std_o;
+	size_t len, var, count;
 
 	var = n;
 	count = 0;
@@ -19,21 +18,6 @@ int format_u(unsigned int n)
 	} while (var != 0);
 
 	len = count;
-
-	std_o = malloc(sizeof(char) * len);
-	j = count - 1;
-	do {
-		std_o[j] = (n % 10) + 48;
-		n /= 10;
-		j--;
-	} while (n != 0);
-
-	j = 0;
-	while (j < len)
-	{
-		format_c(std_o[j]);
-		j++;
-	}
-	free(std_o);
+	format_d(n);
 	return (len);
 }
